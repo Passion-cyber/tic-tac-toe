72ac26343f3f7501c5ff78f5c9d7efce78686aea
PassionCyber <chineduibelachi@gmail.com>
2025-03-10 22:38:04 +0100

    Made new changes and updated the check game status

---
+++ JS/functions.js
@@ -0,0 +1,6 @@
+// This function basically checks if the game has been won by any of the players
+const checkGameStatus = (firstPosition, secondPosition, thirdPosition) => {
+
+    if (dynamicText .textContent.includes("You won the game 😍"))
+  console.log("Checking status of game");
+};
--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,33 @@
-let lastValue = "O";
 let firstValue = "X";
+let lastValue = "O";
 
 let dynamicText = document.getElementById("display");
-let buttons = document.querySelectorAll(".box");
+// let buttons = document.querySelectorAll(".box");
+let buttons = document.getElementsByClassName("box");
+let restartButton = document.getElementById("restartButton");
+
+// console.log("buttons:", buttons);
+// console.log("dynamic text:", dynamicText);
+// console.log("restart button:", restartButton);
+
+for (let button of buttons) {
+  button.addEventListener("click", () => {
+    if (button.textContent !== "") {
+      return;
+    }
+
+    if (lastValue === "O") {
+      button.textContent = "X";
+      lastValue = "X";
+      dynamicText.textContent = `Player 2's turn to play`;
+    //   checkGameStatus();
+    } else {
+      button.textContent = "O";
+      lastValue = "O";
+      dynamicText.textContent = `Player 1's turn to play`;
+    //   checkGameStatus();
+    }
+
+
+  });
+}
