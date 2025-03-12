38c6325369a97ad9bc4cb1fe2596b419e8004588
PassionCyber <chineduibelachi@gmail.com>
2025-03-12 21:18:43 +0100

    Made changes to disable buttons when game is won

--- JS/functions.js
+++ JS/functions.js
@@ -1,6 +1,44 @@
 // This function basically checks if the game has been won by any of the players
 const checkGameStatus = (firstPosition, secondPosition, thirdPosition) => {
+  if (dynamicText.textContent.includes("You won the game 😍"))
+    console.log("Checking status of game");
 
-    if (dynamicText .textContent.includes("You won the game 😍"))
-  console.log("Checking status of game");
+  if (
+    buttons[firstPosition].textContent === "X" &&
+    buttons[secondPosition].textContent === "X" &&
+    buttons[thirdPosition].textContent === "X"
+  ) {
+    disableButtonsWhenGameIsWon("Player One has won the game");
+    return;
+  } else if (
+    buttons[firstPosition].textContent === "O" &&
+    buttons[secondPosition].textContent === "O" &&
+    buttons[thirdPosition].textContent === "O"
+  ) {
+    disableButtonsWhenGameIsWon("Player Two has won the game");
+  } else {
+    checkDrawStatus();
+  }
 };
+
+const disableButtonsWhenGameIsWon = (text) => {
+  for (let button of buttons) {
+    if (button.textContent === "") {
+      button.toggleAttribute("disabled");
+    }
+  }
+  dynamicText.textContent = text;
+};
+
+const checkDrawStatus = () => {
+  for (let button of buttons) {
+    if (button.textContent === "") {
+      return;
+    }
+  }
+  dynamicText.textContent = "It is a draw! 👍";
+};
+
+// const numbersArray = [1, 2, 3, 4, 5, 6, 7];
+
+// console.log(numbersArray[5]);
